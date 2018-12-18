c27fbd5fec3929bb2afb3cc8455feb842dc194a8
laurentPruvost <laurent.pruvost@ymail.com>
2018-12-18 11:18:44 +0100

    C1

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace testJenjins
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine(CreateMessage());
+        }
+
+        public static string CreateMessage()
+        {
+            return "hello";
+        }
+    }
+}
